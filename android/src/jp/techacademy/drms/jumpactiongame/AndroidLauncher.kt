fd8a919565272e369e31807d4dd49ab1bad88dfb
dreamS <tnstarktig@gmail.com>
2020-06-14 21:52:14 +0900

    Initial commit, confirmed before [4.0]

---
+++ android/src/jp/techacademy/drms/jumpactiongame/AndroidLauncher.kt
@@ -0,0 +1,17 @@
+package jp.techacademy.drms.jumpactiongame
+
+
+import android.os.Bundle
+
+import com.badlogic.gdx.backends.android.AndroidApplication
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
+import jp.techacademy.drms.jumpactiongame.JumpActionGame
+
+
+class AndroidLauncher : AndroidApplication() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val config = AndroidApplicationConfiguration()
+        initialize(JumpActionGame(), config)
+    }
+}
